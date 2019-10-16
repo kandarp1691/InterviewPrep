6e2c7bebfcb345b5ba94783ee9cd45cea98bd7a0
Kandarp Gandhi <c-kandarp.gandhi@charter.com>
2019-10-16 09:23:55 -0600

    add a few more examples

---
+++ LeetCode/houseRobber.py
@@ -0,0 +1,15 @@
+def rob(nums):
+    sum = 0
+    i = 0
+    j = 0
+    s1 = 0
+    s2 = 0
+    while i <= len(nums)-1:
+        if i!=0:
+            s1 = s1 + next
+
+
+
+
+nums = [2,7,9,3,1]
+print rob(nums)
--- LeetCode/minStack.py
+++ LeetCode/minStack.py
@@ -4,7 +4,7 @@
 # pop() -- Removes the element on top of the stack.
 # top() -- Get the top element.
 # getMin() -- Retrieve the minimum element in the stack.
-
+import sys
 class MinStack(object):
 
     def __init__(self):
@@ -12,32 +12,63 @@ class MinStack(object):
         initialize your data structure here.
         """
         self.stack = []
-        self.min = None
+        self.min = sys.maxint
+        self.minlist = []
 
     def push(self, x):
         """
         :type x: int
         :rtype: None
         """
+        minVal = self.min if not self.minlist else self.minlist[-1]
+        if x <= minVal:
+            self.minlist.append(x)
+        self.stack.append(x)
 
     def pop(self):
         """
         :rtype: None
         """
+        if self.stack[-1] == self.getMin():
+            self.minlist.pop(-1)
+        self.stack = self.stack[:-1]
+
 
     def top(self):
         """
         :rtype: int
         """
+        return self.stack[-1]
 
     def getMin(self):
         """
         :rtype: int
         """
+        print self.minlist
+        return self.minlist[-1] if self.minlist else []
 
 # Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(x)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
+obj = MinStack()
+obj.push(2147483646)
+obj.push(2147483646)
+obj.push(2147483647)
+print obj.top()
+obj.pop()
+print obj.getMin()
+obj.pop()
+print obj.getMin()
+obj.pop()
+obj.push(2147483647)
+print obj.top()
+print obj.getMin()
+obj.push(-2147483648)
+print obj.top()
+print obj.getMin()
+obj.pop()
+print obj.getMin()
+
+
+# ["MinStack","push","push","push","top","pop","getMin","pop","getMin","pop","push","top","getMin","push","top","getMin","pop","getMin"]
+# [[],[2147483646],[2147483646],[2147483647],[],[],[],[],[],[],[2147483647],[],[],[-2147483648],[],[],[],[]]
+
+#[null,null,null,null,2147483647,null,2147483646,null,2147483646,null,null,2147483647,2147483647,null,-2147483648,-2147483648,null,2147483647]
