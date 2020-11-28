9cc97f70bad01b7aa3f10a36b58613643c631780
kgandhi@mathworks.com <kandarp1691@gmail.com>
2020-11-28 13:25:19 -0500

    add ignore file

--- Hackerrank/python/LinkedList/Driver.py
+++ Hackerrank/python/LinkedList/Driver.py
@@ -4,18 +4,8 @@ from Node import *
 sList = LinkedList()
 sList.insert(10)
 sList.insert(11)
-sList.insert(11)
-sList.insert(14)
-sList.insert(14)
-sList.insert(15)
-sList.insert(16)
-
-#sList.print_list()
-#sList.print_middle()
+sList.insert(12)
+sList.insert(13)
 sList.print_list()
-print "----------"
-sList.removeDuplicates()
-sList.print_list()
-sList.last_to_front()
-print '-------------'
+sList.reverse_list()
 sList.print_list()
--- Hackerrank/python/LinkedList/Node.py
+++ Hackerrank/python/LinkedList/Node.py
@@ -1,7 +1,9 @@
+import pdb
+
 class Node:
     def __init__(self, data):
         self.data = data
-        self.next = next
+        self.next = None
 
 
 class LinkedList:
@@ -9,107 +11,69 @@ class LinkedList:
         self.head = None
 
     def insert(self, data):
-        new_node = Node(data)
-        new_node.next = self.head
-        self.head = new_node
+        if self.head is None:
+            self.head = Node(data)
+        else:
+            curr = self.head
+            # pdb.set_trace()
+            while curr.next:
+                curr = curr.next
+            curr.next = Node(data)
 
     def print_list(self):
-        current = self.head
-        while current:
-            print current.data
-            current = current.next
-
-    def get_size(self):
-        current = self.head
-        count = 0
-        while current:
-            count = count + 1
-            current = current.next
-        return count
+        if self.head is None:
+            print("Empty List")
+        else:
+            curr = self.head
+            res = []
+            while curr is not None:
+                res.append(curr.data)
+                curr = curr.next
+            print res
 
     def print_middle(self):
-        pt1 = self.head
-        pt2 = self.head
-
-        while pt1 and pt1.next:
-            pt2 = pt2.next
-            pt1 = pt1.next.next
-
-        print pt2.data
+        if self.head is None  or self.head.next is None:
+            return self.head
+        fast = self.head
+        slow = self.head
+        while fast.next is not None and fast.next.next is not None:
+            fast = fast.next.next
+            slow = slow.next
+        print slow.data
+        
 
     def nth_from_end(self, nth):
-        pt1 = self.head
-        pt2 = self.head
-
-        while nth-1 >= 0:
-            pt1 = pt1.next
+        if nth < 0:
+            print "invalid entry"
+        fast = self.head
+        slow = self.head
+        while nth is not 1:
+            fast = fast.next
             nth = nth - 1
-
-        while pt1:
-            pt1 = pt1.next
-            pt2 = pt2.next
-
-        print pt2.data
+        while fast.next is not None:
+            fast = fast.next
+            slow = slow.next
+        print slow.data
 
     def reverse_list(self):
-        current = self.head
+        curr = self.head
         nextNode = self.head.next
-        current.next = None
+        curr.next = None
         while nextNode:
-            loopNode = nextNode.next
-            nextNode.next = current
-            current = nextNode
-            nextNode = loopNode
-        self.head = current
+            loop = nextNode.next
+            nextNode.next = curr
+            curr = nextNode
+            nextNode = loop
+        self.head = curr
+
 
     def removeDuplicates(self):
-        current = self.head
-        while current.next:
-            if current.data == current.next.data:
-                current.next = current.next.next
-            else:
-                current = current.next
+        if self.head is None:
+            return self.head
+        
 
-    def last_to_front(self):
-        if self.head == None or self.head.next == None:
-            return
-        pt1 = self.head
-        pt2 = self.head
+    # def last_to_front(self):
 
-        pt1 = pt1.next
 
-        while pt1.next:
-            pt1 = pt1.next
-            pt2 = pt2.next
-
-        pt2.next = None
-        pt1.next = self.head
-        self.head = pt1
-
-    def add_two_numbers_by_lists(self, first, second):
-        temp = None
-        prev = None
-        carry = 0
-
-        while first is not None or second is not None:
-            fdata = 0 if first is None else first.data
-            sdata = 0 if second is None else second.data
-            sum = fdata + sdata + carry
-
-            carry = 1 if sum >=10 else 0
-            sum = sum if sum < 10 else sum%10
-
-            temp = Node(sum)
-
-            if self.head is None:
-                temp = self.head
-            else:
-                prev = temp
-
-            if first is not None:
-                first = first.next
-            if second is not None:
-                second = second.next
-
-        if carry > 0:
-            temp = Node(carry)
+    # def add_two_numbers_by_lists(self, first, second):
+  
