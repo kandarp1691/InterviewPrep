9e67c72c9eb72e2ab6cfb1b1a9806f9ed0bcd13f
Kandarp Gandhi <kgandhi@mathworks.com>
2021-04-16 15:15:32 -0400

    group questions

---
+++ LeetCode/Medium/groupAnagrams.py
@@ -0,0 +1,11 @@
+import collections
+def groupAnagrams(strs):
+    ans = collections.defaultdict(list)
+    for s in strs:
+        count = [0] * 26
+        for c in s:
+            count[ord(c) - ord('a')] += 1
+        ans[tuple(count)].append(s)
+    return ans.values()
+
+print groupAnagrams(["eat","tea","tan","ate","nat","bat"])
---
+++ LeetCode/Medium/rotateMatrix.py
@@ -0,0 +1,5 @@
+def rotateMatrix(mat):
+    i = 0
+    j = 0
+    while True:
+        mat[i+]
